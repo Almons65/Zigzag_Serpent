c5c1de87ce0ea9847ecb0d11b410decca0c8990a
Almons65 <adisorn.almond@gmail.com>
2025-05-12 15:29:12 +0700

    Add files via upload

---
+++ target/debug/build/zerocopy-3e5e22dc32edf8f8/build_script_build-3e5e22dc32edf8f8.d
@@ -0,0 +1,5 @@
+C:\Users\adiso\OneDrive\Documents\Coding\Rust\Zigzag Serpent (Rust Mini-Project)\target\debug\build\zerocopy-3e5e22dc32edf8f8\build_script_build-3e5e22dc32edf8f8.exe: C:\Users\adiso\.cargo\registry\src\index.crates.io-6f17d22bba15001f\zerocopy-0.8.25\build.rs
+
+C:\Users\adiso\OneDrive\Documents\Coding\Rust\Zigzag Serpent (Rust Mini-Project)\target\debug\build\zerocopy-3e5e22dc32edf8f8\build_script_build-3e5e22dc32edf8f8.d: C:\Users\adiso\.cargo\registry\src\index.crates.io-6f17d22bba15001f\zerocopy-0.8.25\build.rs
+
+C:\Users\adiso\.cargo\registry\src\index.crates.io-6f17d22bba15001f\zerocopy-0.8.25\build.rs:
